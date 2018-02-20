6e215ab18507f4f0c1317c91118a287c55a6fc63
Andrey Scherbovich <andrey@gnosis.pm>
2018-02-20 11:39:39 +0100

    WA-154 Bootstrap project

---
+++ safe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  safe
+//
+//  Created by Andrey Scherbovich on 20.02.18.
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
