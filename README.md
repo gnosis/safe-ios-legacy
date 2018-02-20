1a78c715a833a3774dbdee552fa6a1d75759b182
Andrey Scherbovich <andrey@gnosis.pm>
2018-02-20 15:28:21 +0100

    WA-154 Added config files and Fabric

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # safe-ios
+
+# Getting Started
+
+To set up all project dependencies, run from the project directory in terminal:
+
+```
+$> ./bootstrap.sh
+```
+
---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+set -ex
+
+if ! which brew > /dev/null; then
+  echo "Installing HomeBrew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if ! which swiftlint > /dev/null; then
+  echo "Installing SwiftLint"
+  brew install swiftlint
+fi
+
+if ! which rbenv > /dev/null; then
+  echo "Installing rbenv"
+  brew install rbenv
+fi
+
+if ! rbenv versions | grep $(cat .ruby-version) > /dev/null; then
+  echo "Installing Ruby $(cat .ruby-version)"
+  rbenv install $(cat .ruby-version)
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+if ! rbenv which bundle > /dev/null; then
+  echo "Installing bundler"
+  gem install bundler
+fi
+
+echo "Running bundle install"
+bundle install
+
+echo "Bootstrapping complete!"
