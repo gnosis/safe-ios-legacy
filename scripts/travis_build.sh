e2de7246a4e9d51848e650f2762ed54f8c245c04
DmitryBespalov <dima.bespalov@gmail.com>
2018-02-23 11:09:43 +0100

    WA-154 Test run of ui tests

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,9 +1,9 @@
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+# if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+#   if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
     bundle exec fastlane ui_test
-  else
-    bundle exec fastlane fabric
-  fi
-else
-  bundle exec fastlane test
-fi
+#   else
+#     bundle exec fastlane fabric
+#   fi
+# else
+#   bundle exec fastlane test
+# fi
