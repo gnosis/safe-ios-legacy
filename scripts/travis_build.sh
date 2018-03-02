904604b9e5449cbdeccef1e62eb7f179898dee6f
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-03-02 14:42:09 +0100

    WA-158 Enable normal ci build flow

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,11 +1,10 @@
-# if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-#   # Cron job is triggerred daily
-#   if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-#     bundle exec fastlane ui_test
-#   else
-#     bundle exec fastlane fabric
-#   fi
-# else
-#   bundle exec fastlane test
-# fi
-bundle exec fastlane fabric
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  # Cron job is triggerred daily
+  if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+    bundle exec fastlane ui_test
+  else
+    bundle exec fastlane fabric
+  fi
+else
+  bundle exec fastlane test
+fi
