5815136248193c5fd33ad1fa9bb63ace5b777a8f
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-03-02 14:32:45 +0100

    WA-158 Attempt to fix master

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,10 +1,11 @@
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  # Cron job is triggerred daily
-  if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-    bundle exec fastlane ui_test
-  else
-    bundle exec fastlane fabric
-  fi
-else
-  bundle exec fastlane test
-fi
+# if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+#   # Cron job is triggerred daily
+#   if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+#     bundle exec fastlane ui_test
+#   else
+#     bundle exec fastlane fabric
+#   fi
+# else
+#   bundle exec fastlane test
+# fi
+bundle exec fastlane fabric
