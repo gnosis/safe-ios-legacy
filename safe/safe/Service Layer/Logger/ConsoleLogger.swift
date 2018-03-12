cfdc09f954f53cb7af2b73ed0e1f2b22582f9b62
Andrey Scherbovich <andrey@gnosis.pm>
2018-03-12 15:56:33 +0100

    WA-215 WIP: Console Logger

---
+++ safe/safe/Service Layer/Logger/ConsoleLogger.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2018 Gnosis. All rights reserved.
+//
+
+import Foundation
+
+final class ConsoleLogger: Logger {
+
+    func log(_ message: String, file: StaticString, line: UInt, function: StaticString) {
+        
+    }
+
+}
