6ff17a8bcbb8c62117f1585f2a74b968a597e7c7
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-03-29 14:48:29 +0200

    WA-165 Added encryption service and account factory

---
+++ safe/safe/DomainModelLayer/EthereumAccount.swift
@@ -0,0 +1,50 @@
+//
+//  Copyright © 2018 Gnosis. All rights reserved.
+//
+
+import Foundation
+
+protocol EthereumAccountProtocol {
+
+    var address: EthereumAddress { get }
+    var mnemonic: Mnemonic { get }
+    var privateKey: PrivateKey { get }
+    var publicKey: PublicKey { get }
+
+}
+
+struct ExternallyOwnedAccount: EthereumAccountProtocol {
+
+    var address: EthereumAddress
+    var mnemonic: Mnemonic
+    var privateKey: PrivateKey
+    var publicKey: PublicKey
+
+}
+
+protocol EthereumAccountFactoryProtocol {
+
+    func generateAccount() -> EthereumAccountProtocol
+
+}
+
+class EthereumAccountFactory: EthereumAccountFactoryProtocol {
+
+    private let encryptionService: EncryptionServiceProtocol
+
+    init(service: EncryptionServiceProtocol = EncryptionService()) {
+        self.encryptionService = service
+    }
+
+    func generateAccount() -> EthereumAccountProtocol {
+        let mnemonic = encryptionService.generateMnemonic()
+        let privateKey = encryptionService.derivePrivateKey(from: mnemonic)
+        let publicKey = encryptionService.derivePublicKey(from: privateKey)
+        let address = encryptionService.deriveEthereumAddress(from: publicKey)
+        return ExternallyOwnedAccount(address: address,
+                                      mnemonic: mnemonic,
+                                      privateKey: privateKey,
+                                      publicKey: publicKey)
+    }
+
+}
--- safe/safe/ServiceLayer/EncryptionService.swift
+++ safe/safe/ServiceLayer/EncryptionService.swift
@@ -7,35 +7,47 @@ import EthereumKit
 
 protocol EncryptionServiceProtocol {
 
-    func encrypt(_ data: String, _ key: PublicKey) -> Data
-
-    func decrypt(_ data: Data, _ key: PrivateKey) -> Data
-
-    func derivePrivateKey(from mnemonic: Mnemonic) -> PrivateKey?
-
-    func deriveEthereumAddress(from key: PublicKey) -> EthereumAddress
-
     func generateMnemonic() -> Mnemonic
+    func derivePrivateKey(from mnemonic: Mnemonic) -> PrivateKey
+    func derivePublicKey(from key: PrivateKey) -> PublicKey
+    func deriveEthereumAddress(from key: PublicKey) -> EthereumAddress
+    func sign(_ data: Data, _ key: PrivateKey) -> Signature
+    func isValid(signature: Signature, for data: Data, with key: PublicKey) -> Bool
 
 }
 
-final class EncryptionService {
+final class EncryptionService: EncryptionServiceProtocol {
 
-    func encrypt(_ data: String, _ key: PublicKey) -> Data {
-        return data.data(using: .utf8) ?? Data()
+    func sign(_ data: Data, _ key: PrivateKey) -> Signature {
+        let sig = try! EthereumKit.Crypto.sign(hash(data), privateKey: key.data)
+        return Signature(data: sig)
     }
 
-    func decrypt(_ data: Data, _ key: PrivateKey) -> Data {
-        return data
+    func hash(_ data: Data) -> Data {
+        return Crypto.hashSHA3_256(data)
     }
 
-    func derivePrivateKey(from mnemonic: Mnemonic) -> PrivateKey? {
+    func isValid(signature: Signature, for data: Data, with key: PublicKey) -> Bool {
+        return Crypto.isValid(signature: signature.data,
+                              of: hash(data),
+                              publicKey: key.data,
+                              compressed: key.isCompressed)
+    }
+
+    func derivePrivateKey(from mnemonic: Mnemonic) -> PrivateKey {
         let seed = EthereumKit.Mnemonic.createSeed(mnemonic: mnemonic.words)
         return PrivateKey(data: HDPrivateKey(seed: seed, network: .main).raw)
     }
 
+    func derivePublicKey(from key: PrivateKey) -> PublicKey {
+        let isCompressed = true
+        return PublicKey(data: EthereumKit.Crypto.generatePublicKey(data: key.data, compressed: isCompressed),
+                         compressed: isCompressed)
+    }
+
     func deriveEthereumAddress(from key: PublicKey) -> EthereumAddress {
-        return EthereumAddress()
+        let data = EthereumKit.Address(string: EthereumKit.PublicKey(raw: key.data).generateAddress()).data
+        return EthereumAddress(data: data)
     }
 
     func generateMnemonic() -> Mnemonic {
