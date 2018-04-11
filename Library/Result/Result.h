bd902ab0e3dbab3606174c899364c2b273946ea1
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-04-11 14:57:30 +0200

    WA-284 Added Result at 3.2.4

---
+++ Library/Result/Result.h
@@ -0,0 +1,15 @@
+//
+//  
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Result.
+FOUNDATION_EXPORT double ResultVersionNumber;
+
+//! Project version string for Result.
+FOUNDATION_EXPORT const unsigned char ResultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Result/PublicHeader.h>
+
+
