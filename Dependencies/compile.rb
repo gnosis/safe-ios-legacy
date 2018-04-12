e5d3cdc3cfb9d0725f6768d24b002767680d1758
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-04-12 08:33:05 +0200

    WA-284 Updated paths

--- Dependencies/compile.rb
+++ Dependencies/compile.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 sdks = ["iphoneos", "iphonesimulator"]
-deps = YAML.load_file('Library.yml')
+deps = YAML.load_file('Dependencies.yml')
 deps.each do |dependency|
     if dependency.is_a?(Hash)
         project = dependency.first.first
@@ -11,11 +11,11 @@ deps.each do |dependency|
         target = dependency
     end
     sdks.each do |sdk|
-        cmd = ["xcodebuild -project #{project}/src/#{project}.xcodeproj/",
+        cmd = ["xcodebuild -project #{project}/#{project}.xcodeproj/",
             "-target #{target} -sdk #{sdk}",
-            "SYMROOT='${SRCROOT}/../../../Build/'", 
-            "DSTROOT='${SRCROOT}/../../${PLATFORM_NAME}'",
-            "FRAMEWORK_SEARCH_PATHS='$(inherited) ${SRCROOT}/../../${PLATFORM_NAME}'",
+            "SYMROOT='${SRCROOT}/../../Build/'", 
+            "DSTROOT='${SRCROOT}/../../Library/${PLATFORM_NAME}'",
+            "FRAMEWORK_SEARCH_PATHS='$(inherited) ${SRCROOT}/../../Library/${PLATFORM_NAME}'",
             "INSTALL_PATH=/ DWARF_DSYM_FOLDER_PATH='${DSTROOT}'",
             "VALID_ARCHS='arm64 armv7 armv7s i386 x86_64'"
             "SKIP_INSTALL=NO install"].join(" ")
