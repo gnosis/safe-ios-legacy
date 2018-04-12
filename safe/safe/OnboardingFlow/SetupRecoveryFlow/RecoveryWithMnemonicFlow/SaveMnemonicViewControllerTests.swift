0d2579669bc098a8a58e6c0deaa6e1c676196b43
Andrey Scherbovich <andrey@gnosis.pm>
2018-04-12 15:27:50 +0200

    WA-272 Set mnemonic in SaveMnemonicVC

--- safe/safe/OnboardingFlow/SetupRecoveryFlow/RecoveryWithMnemonicFlow/SaveMnemonicViewControllerTests.swift
+++ safe/safe/OnboardingFlow/SetupRecoveryFlow/RecoveryWithMnemonicFlow/SaveMnemonicViewControllerTests.swift
@@ -4,8 +4,10 @@
 
 import XCTest
 @testable import safe
+import IdentityAccessDomainModel
+import CommonTestSupport
 
-class SaveMnemonicViewControllerTests: XCTestCase {
+class SaveMnemonicViewControllerTests: SafeTestCase {
 
     // swiftlint:disable weak_delegate
     private let delegate = MockSaveMnemonicDelegate()
@@ -19,6 +21,32 @@ class SaveMnemonicViewControllerTests: XCTestCase {
 
     func test_canCreate() {
         XCTAssertNotNil(controller)
+        XCTAssertNotNil(controller.titleLabel)
+        XCTAssertNotNil(controller.mnemonicCopyableLabel)
+        XCTAssertNotNil(controller.saveButton)
+        XCTAssertNotNil(controller.descriptionLabel)
+        XCTAssertNotNil(controller.continueButton)
+    }
+
+    func test_viewDidLoad_setsCorrectMnemonic() throws {
+        let mnemonicStr = "test mnemonic"
+        try secureStore.saveMnemonic(Mnemonic(mnemonicStr))
+        controller.viewDidLoad()
+        XCTAssertEqual(controller.mnemonicCopyableLabel.text, mnemonicStr)
+    }
+
+    func test_viewDidLoad_dismissesIfNoEOAisSet() throws {
+        try secureStore.removeMnemonic()
+        guard let window = UIApplication.shared.keyWindow else {
+            XCTFail("Must have active window")
+            return
+        }
+        window.rootViewController?.present(controller, animated: false)
+        delay()
+        XCTAssertNotNil(controller.view.window)
+        controller.viewDidLoad()
+        delay(1)
+        XCTAssertNil(controller.view.window)
     }
 
 }
