fcc519c324d1c9cead9cb456e0654490a855e4ef
Andrey Scherbovich <andrey@gnosis.pm>
2018-04-13 10:18:43 +0200

    WA-272 add random words to check to controller

--- safe/safe/OnboardingFlow/SetupRecoveryFlow/RecoveryWithMnemonicFlow/ConfirmMnemonicViewController.swift
+++ safe/safe/OnboardingFlow/SetupRecoveryFlow/RecoveryWithMnemonicFlow/ConfirmMnemonicViewController.swift
@@ -4,6 +4,7 @@
 
 import UIKit
 import safeUIKit
+import IdentityAccessDomainModel
 
 protocol ConfirmMnemonicDelegate: class {
     func didConfirm()
@@ -19,25 +20,46 @@ final class ConfirmMnemonicViewController: UIViewController {
     @IBOutlet weak var secondWordTextInput: TextInput!
     @IBOutlet weak var confirmButton: UIButton!
 
-    weak var delegate: ConfirmMnemonicDelegate?
+    private(set) weak var delegate: ConfirmMnemonicDelegate?
+    private(set) var mnemonic: Mnemonic!
+    private(set) var firstMnemonicWordToCheck = ""
+    private(set) var secondMnemonicWordToCheck = ""
 
     @IBAction func confirm(_ sender: Any) {
     }
 
-    static func create(delegate: ConfirmMnemonicDelegate) -> ConfirmMnemonicViewController {
+    static func create(delegate: ConfirmMnemonicDelegate, mnemonic: Mnemonic) -> ConfirmMnemonicViewController {
         let controller = StoryboardScene.SetupRecovery.confirmMnemonicViewController.instantiate()
         controller.delegate = delegate
+        controller.mnemonic = mnemonic
         return controller
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let mnemonic = mnemonic, mnemonic.words.count > 1 else {
+            dismiss(animated: true)
+            return
+        }
+        (firstMnemonicWordToCheck, secondMnemonicWordToCheck) = twoRandomWords()
+        firstWordNumberLabel.text = "\(mnemonic.words.index(of: firstMnemonicWordToCheck)! + 1)."
+        secondWordNumberLabel.text = "\(mnemonic.words.index(of: secondMnemonicWordToCheck)! + 1)."
         titleLabel.text = NSLocalizedString("recovery.confirm_mnemonic.title",
                                             comment: "Title for confirm mnemonic view controller")
         descriptionLabel.text = NSLocalizedString("recovery.confirm_mnemonic.description",
                                                   comment: "Description for confirm mnemonic view controller")
         confirmButton.setTitle(NSLocalizedString("recovery.confirm_mnemonic.confirm",
-                                                 comment: "Confirm button"), for: .normal)        
+                                                 comment: "Confirm button"), for: .normal)
+    }
+
+    private func twoRandomWords() -> (String, String) {
+        var words = mnemonic.words
+        let firstIndex = Int(arc4random_uniform(UInt32(words.count)))
+        let firstWord = words[firstIndex]
+        words.remove(at: firstIndex)
+        let secondIndex = Int(arc4random_uniform(UInt32(words.count)))
+        let secondWord = words[secondIndex]
+        return (firstWord, secondWord)
     }
 
 }
--- safe/safe/OnboardingFlow/SetupRecoveryFlow/RecoveryWithMnemonicFlow/ConfirmMnemonicViewControllerTests.swift
+++ safe/safe/OnboardingFlow/SetupRecoveryFlow/RecoveryWithMnemonicFlow/ConfirmMnemonicViewControllerTests.swift
@@ -4,17 +4,19 @@
 
 import XCTest
 @testable import safe
+import IdentityAccessDomainModel
+import CommonTestSupport
 
 class ConfirmMnemonicViewControllerTests: XCTestCase {
 
     // swiftlint:disable weak_delegate
     private let delegate = MockConfirmMnemonicDelegate()
     private var controller: ConfirmMnemonicViewController!
+    private let mnemonic = Mnemonic("some mnemonic with several words")
 
     override func setUp() {
         super.setUp()
-        controller = ConfirmMnemonicViewController.create(delegate: delegate)
-        controller.loadViewIfNeeded()
+        createController(mnemonic)
     }
 
     func test_canCreate() {
@@ -27,6 +29,67 @@ class ConfirmMnemonicViewControllerTests: XCTestCase {
         XCTAssertNotNil(controller.secondWordTextInput)
         XCTAssertNotNil(controller.confirmButton)
         XCTAssertTrue(controller.delegate === delegate)
+        XCTAssertEqual(mnemonic, controller.mnemonic)
+    }
+
+    func test_viewDidLoad_setsRandomCheckingWords() {
+        assertRandomWords()
+        let smallMnemonic = Mnemonic("two words")
+        createController(smallMnemonic)
+        assertRandomWords()
+    }
+
+    func test_viewDidLoad_dismissesIfMnemonicIsNil() throws {
+        let controller = ConfirmMnemonicViewController()
+        createWindow(controller)
+        controller.viewDidLoad()
+        delay(1)
+        XCTAssertNil(controller.view.window)
+    }
+
+    func test_viewDidLoad_dismissesIfMnemonicHasLessThanTwoWords() throws {
+        let mnemonic = Mnemonic("word")
+        createController(mnemonic)
+        createWindow(controller)
+        controller.viewDidLoad()
+        delay(1)
+        XCTAssertNil(controller.view.window)
+    }
+
+    func test_viewDidLoad_setsCorrectWordsLabelText() {
+        controller.viewDidLoad()
+        let firstWordIndex = mnemonic.words.index(of: controller.firstMnemonicWordToCheck)!
+        let secondWordIndex = mnemonic.words.index(of: controller.secondMnemonicWordToCheck)!
+        XCTAssertEqual("\(firstWordIndex + 1).", controller.firstWordNumberLabel.text)
+        XCTAssertEqual("\(secondWordIndex + 1).", controller.secondWordNumberLabel.text)
+    }
+
+}
+
+extension ConfirmMnemonicViewControllerTests {
+
+    private func createController(_ mnemonic: Mnemonic) {
+        controller = ConfirmMnemonicViewController.create(delegate: delegate, mnemonic: mnemonic)
+        controller.loadViewIfNeeded()
+    }
+
+    private func createWindow(_ controller: UIViewController) {
+        guard let window = UIApplication.shared.keyWindow else {
+            XCTFail("Must have active window")
+            return
+        }
+        window.rootViewController?.present(controller, animated: false)
+        delay()
+        XCTAssertNotNil(controller.view.window)
+    }
+
+    private func assertRandomWords() {
+        for _ in 0...100 {
+            controller.viewDidLoad()
+            XCTAssertNotEqual(controller.firstMnemonicWordToCheck, controller.secondMnemonicWordToCheck)
+            XCTAssertTrue(controller.mnemonic.words.contains(controller.firstMnemonicWordToCheck))
+            XCTAssertTrue(controller.mnemonic.words.contains(controller.secondMnemonicWordToCheck))
+        }
     }
 
 }
