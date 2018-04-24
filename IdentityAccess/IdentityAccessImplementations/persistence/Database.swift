57f9ad84e771c69d8ee79acb5f2a12aaec711852
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-04-24 12:49:33 +0200

    IOS-271 whitespace

--- IdentityAccess/IdentityAccessImplementations/persistence/Database.swift
+++ IdentityAccess/IdentityAccessImplementations/persistence/Database.swift
@@ -5,32 +5,42 @@
 import Foundation
 
 public protocol Database {
+
     func create() throws
     func destroy() throws
     func connection() throws -> Connection
     func close(_ connection: Connection) throws
+
 }
 
 public protocol Connection {
+
     func prepare(statement: String) throws -> Statement
+
 }
 
 public protocol Statement {
+
     func set(_ value: String, at index: Int) throws
     func set(_ value: Int, at index: Int) throws
     func set(_ value: Double, at index: Int) throws
     func setNil(at index: Int) throws
+
     func set(_ value: String, forKey key: String) throws
     func set(_ value: Int, forKey key: String) throws
     func set(_ value: Double, forKey key: String) throws
     func setNil(forKey key: String) throws
+
     @discardableResult
     func execute() throws -> ResultSet?
+
 }
 
 public protocol ResultSet {
+
     func advanceToNextRow() throws -> Bool
     func string(at index: Int) -> String?
     func int(at index: Int) -> Int
     func double(at index: Int) -> Double
+
 }
