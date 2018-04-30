9c859e7d5719275e5a8ba838ab9e94a300c65f37
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-04-30 13:41:40 +0200

    WA-274 Fixed user tests

--- IdentityAccess/IdentityAccessDomainModel/User.swift
+++ IdentityAccess/IdentityAccessDomainModel/User.swift
@@ -12,14 +12,6 @@ public class User: IdentifiableEntity<UserID> {
     public private(set) var password: String = ""
     public private(set) var sessionID: SessionID?
 
-    public enum Error: Swift.Error, Hashable {
-        case emptyPassword
-        case passwordTooShort
-        case passwordTooLong
-        case passwordMissingCapitalLetter
-        case passwordMissingDigit
-    }
-
     public static func ==(lhs: User, rhs: User) -> Bool {
         return lhs.id == rhs.id
     }
@@ -30,12 +22,6 @@ public class User: IdentifiableEntity<UserID> {
     }
 
     func changePassword(old: String, new password: String) throws {
-        // FIXME: password is encrypted
-//        try assertArgument(!password.isEmpty, Error.emptyPassword)
-//        try assertArgument(password.count >= 6, Error.passwordTooShort)
-//        try assertArgument(password.count <= 100, Error.passwordTooLong)
-//        try assertArgument(password.hasUppercaseLetter, Error.passwordMissingCapitalLetter)
-//        try assertArgument(password.hasDecimalDigit, Error.passwordMissingDigit)
         self.password = password
     }
 
