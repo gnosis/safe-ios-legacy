4ab92cf3881a19e47bd70d2a859bab88e561118c
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-02 11:49:14 +0200

    WA-274 Added app deletion

--- safe/safeUITests/NewSafeScreenUITests.swift
+++ safe/safeUITests/NewSafeScreenUITests.swift
@@ -4,22 +4,45 @@
 
 import XCTest
 
-class NewSafeScreenUITests: XCTestCase {
+class UITestCase: XCTestCase {
 
     let application = Application()
     let password = "11111A"
-    let screen = NewSafeScreen()
 
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
+    }
+
+    func givenUnlockedAppSetup() {
         application.resetAllContentAndSettings()
         application.setPassword(password)
         application.start()
         let unlock = UnlockScreen()
         unlock.enterPassword(password)
+    }
+
+    func givenNewSafeSetup() {
+        givenUnlockedAppSetup()
         let setupOptions = SetupSafeOptionsScreen()
         setupOptions.newSafe.tap()
+    }
+
+    func givenBrowserExtensionSetup() {
+        givenNewSafeSetup()
+        let newSafe = NewSafeScreen()
+        newSafe.browserExtension.element.tap()
+    }
+
+}
+
+class NewSafeScreenUITests: UITestCase {
+
+    let screen = NewSafeScreen()
+
+    override func setUp() {
+        super.setUp()
+        givenNewSafeSetup()
     }
 
     func test_contents() {
---
+++ safe/safeUITests/PairWithBrowserExtensionScreenUITests.swift
@@ -0,0 +1,70 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import XCTest
+import CommonTestSupport
+
+class PairWithBrowserExtensionScreenUITests: UITestCase {
+
+    let screen = PairWithBrowserExtensionScreen()
+
+    override func setUp() {
+        super.setUp()
+        givenBrowserExtensionSetup()
+    }
+
+    override func tearDown() {
+        Springboard.deleteSafeApp()
+        super.tearDown()
+    }
+
+    func test_contents() {
+        XCTAssertExist(screen.qrCodeInput)
+        XCTAssertExist(screen.finishButton)
+        XCTAssertFalse(screen.finishButton.isEnabled)
+    }
+
+    func test_denyCameraAccess() {
+        screen.qrCodeInput.tap()
+    }
+
+}
+
+class Springboard {
+
+    static let springboard = XCUIApplication(privateWithPath: nil, bundleID: "com.apple.springboard")
+
+    class func deleteSafeApp() {
+        guard let springboard = springboard else {
+            preconditionFailure("Failed to find the app")
+        }
+        let terminatedStates: [XCUIApplication.State] = [.unknown, .notRunning]
+        if !terminatedStates.contains(XCUIApplication().state) {
+            XCUIApplication().terminate()
+        }
+        // Resolve the query for the springboard rather than launching it
+        springboard.resolve()
+
+        let safeIcons = springboard.icons.matching(identifier: "Safe")
+        for _ in (0..<safeIcons.count) {
+            let icon = safeIcons.element(boundBy: 0)
+            if icon.exists {
+                let iconFrame = icon.frame
+                let springboardFrame = springboard.frame
+                icon.press(forDuration: 1.3)
+
+                // Tap the little "X" button at approximately where it is. The X is not exposed directly
+                let xOffset = CGVector(dx: (iconFrame.minX + 3) / springboardFrame.maxX,
+                                       dy: (iconFrame.minY + 3) / springboardFrame.maxY)
+                springboard.coordinate(withNormalizedOffset: xOffset).tap()
+
+                delay(1)
+                springboard.buttons["Delete"].tap()
+                delay(2)
+                XCUIDevice.shared.press(.home)
+            }
+        }
+    }
+
+}
