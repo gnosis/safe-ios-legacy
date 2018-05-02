e9a0f6ad6671ac715948367fdfd9b881aaaff54c
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-02 14:55:47 +0200

    WA-274 Added test with deleting the app

--- safe/safeUITests/PairWithBrowserExtensionScreenUITests.swift
+++ safe/safeUITests/PairWithBrowserExtensionScreenUITests.swift
@@ -14,19 +14,34 @@ class PairWithBrowserExtensionScreenUITests: UITestCase {
         givenBrowserExtensionSetup()
     }
 
-    override func tearDown() {
-        Springboard.deleteSafeApp()
-        super.tearDown()
-    }
-
     func test_contents() {
         XCTAssertExist(screen.qrCodeInput)
         XCTAssertExist(screen.finishButton)
         XCTAssertFalse(screen.finishButton.isEnabled)
     }
 
-    func test_denyCameraAccess() {
+    func test_requiresAppReinstalled_denyCameraAccess() {
+        let expectation = self.expectation(description: "Alerts handled")
+        let handler1 = addUIInterruptionMonitor(withDescription: "Camera access") { alert in
+            guard alert.label.localizedCaseInsensitiveContains("would like to access the camera") else { return false }
+            alert.buttons["Don’t Allow"].tap()
+            return true
+        }
+        let handler2 = addUIInterruptionMonitor(withDescription: "Suggestion Alert") { alert in
+            guard alert.label == XCLocalizedString("scanner.camera_access_required.title", table: "safeUIKit") else {
+                return false
+            }
+            alert.buttons[XCLocalizedString("cancel", table: "safeUIKit")].tap()
+            expectation.fulfill()
+            return true
+        }
         screen.qrCodeInput.tap()
+        delay(1)
+        XCUIApplication().tap() // required for alert handlers firing
+        waitForExpectations(timeout: 5)
+        removeUIInterruptionMonitor(handler1)
+        removeUIInterruptionMonitor(handler2)
+        Springboard.deleteSafeApp()
     }
 
 }
