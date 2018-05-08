8d038a9c62281487ff3ff527288281b8c128e1e7
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-08 15:03:58 +0200

    Added wallet states

--- MultisigWallet/MultisigWalletDomainModel/PortfolioTests.swift
+++ MultisigWallet/MultisigWalletDomainModel/PortfolioTests.swift
@@ -7,58 +7,55 @@ import XCTest
 
 class PortfolioTests: XCTestCase {
 
+    var portfolio: Portfolio!
+    var wallet: Wallet!
+    var wallet1: Wallet!
+    var wallet2: Wallet!
+
+    override func setUp() {
+        super.setUp()
+        XCTAssertNoThrow(portfolio = Portfolio(id: try PortfolioID()))
+        XCTAssertNoThrow(wallet = Wallet(id: try WalletID()))
+        XCTAssertNoThrow(wallet1 = Wallet(id: try WalletID()))
+        XCTAssertNoThrow(wallet2 = Wallet(id: try WalletID()))
+
+    }
+
     func test_whenCreated_thenHasID() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
         XCTAssertNotNil(portfolio.id)
     }
 
     func test_whenFirstWalletAdded_thenItIsSelected() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet = Wallet(id: try WalletID())
         try portfolio.addWallet(wallet.id)
         XCTAssertEqual(portfolio.selectedWallet, wallet.id)
     }
 
     func test_whenAddingExistingWallet_thenThrows() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet = Wallet(id: try WalletID())
         try portfolio.addWallet(wallet.id)
         XCTAssertThrowsError(try portfolio.addWallet(wallet.id))
     }
 
     func test_whenRemovingWallet_thenRemoves() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet = Wallet(id: try WalletID())
         try portfolio.addWallet(wallet.id)
         try portfolio.removeWallet(wallet.id)
         XCTAssertNil(portfolio.selectedWallet)
     }
 
     func test_whenAddingMultipleWallets_thenCanFetchAll() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet1 = Wallet(id: try WalletID())
-        let wallet2 = Wallet(id: try WalletID())
         try portfolio.addWallet(wallet1.id)
         try portfolio.addWallet(wallet2.id)
         XCTAssertEqual(portfolio.wallets, [wallet1.id, wallet2.id])
     }
 
     func test_whenRemovingInexistingWallet_thenThrows() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet = Wallet(id: try WalletID())
         XCTAssertThrowsError(try portfolio.removeWallet(wallet.id))
     }
 
     func test_whenSelectingInexistingWallet_thenThrows() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet = Wallet(id: try WalletID())
         XCTAssertThrowsError(try portfolio.selectWallet(wallet.id))
     }
 
     func test_whenSelectingWallet_thenItIsSelected() throws {
-        let portfolio = Portfolio(id: try PortfolioID())
-        let wallet1 = Wallet(id: try WalletID())
-        let wallet2 = Wallet(id: try WalletID())
         try portfolio.addWallet(wallet1.id)
         try portfolio.addWallet(wallet2.id)
         try portfolio.selectWallet(wallet2.id)
--- MultisigWallet/MultisigWalletDomainModel/WalletTests.swift
+++ MultisigWallet/MultisigWalletDomainModel/WalletTests.swift
@@ -7,29 +7,30 @@ import XCTest
 
 class WalletTests: XCTestCase {
 
+    var wallet: Wallet!
+    let owner = Owner(address: BlockchainAddress(value: "My Address"))
+
+    override func setUp() {
+        super.setUp()
+        XCTAssertNoThrow(wallet = Wallet(id: try WalletID()))
+    }
+
     func test_init_whenCreated_thenHasID() throws {
-        let wallet = Wallet(id: try WalletID())
         XCTAssertNotNil(wallet.id)
     }
 
     func test_whenAddingOwner_thenHasOwner() throws {
-        let owner = Owner(address: BlockchainAddress(value: "My Address"))
-        let wallet = Wallet(id: try WalletID())
         try wallet.addOwner(owner, kind: "kind")
         XCTAssertEqual(wallet.owner(kind: "kind"), owner)
     }
 
     func test_whenAddingOwnerTwice_thenThrows() throws {
-        let owner = Owner(address: BlockchainAddress(value: "My Address"))
-        let wallet = Wallet(id: try WalletID())
         try wallet.addOwner(owner, kind: "kind")
         XCTAssertThrowsError(try wallet.addOwner(owner, kind: "kind"))
     }
 
     func test_whenReplacingOwner_thenAnotherOwnerExists() throws {
-        let owner = Owner(address: BlockchainAddress(value: "My Address"))
         let otherOwner = Owner(address: BlockchainAddress(value: "Other"))
-        let wallet = Wallet(id: try WalletID())
         try wallet.addOwner(owner, kind: "kind")
         try wallet.replaceOwner(with: otherOwner, kind: "kind")
         XCTAssertEqual(wallet.owner(kind: "kind"), otherOwner)
@@ -37,22 +38,73 @@ class WalletTests: XCTestCase {
     }
 
     func test_whenReplacingNotExistingOwner_thenThrows() throws {
-        let wallet = Wallet(id: try WalletID())
-        let owner = Owner(address: BlockchainAddress(value: "My Address"))
         XCTAssertThrowsError(try wallet.replaceOwner(with: owner, kind: "kind"))
     }
 
     func test_whenRemovingInexistingOwner_thenThrows() throws {
-        let wallet = Wallet(id: try WalletID())
         XCTAssertThrowsError(try wallet.removeOwner(kind: "kind"))
     }
 
     func test_whenRemovingOwner_thenItDoesNotExist() throws {
-        let owner = Owner(address: BlockchainAddress(value: "My Address"))
-        let wallet = Wallet(id: try WalletID())
         try wallet.addOwner(owner, kind: "kind")
         try wallet.removeOwner(kind: "kind")
         XCTAssertNil(wallet.owner(kind: "kind"))
+    }
+
+    func test_whenCreated_thenInDraftState() throws {
+        XCTAssertEqual(wallet.status, .newDraft)
+    }
+
+    func test_whenDeploymentStarted_thenChangesState() throws {
+        try wallet.startDeployment()
+        XCTAssertEqual(wallet.status, .deploymentPending)
+    }
+
+    func test_whenDeploymentCompleted_thenChangesStatus() throws {
+        try wallet.startDeployment()
+        try wallet.completeDeployment()
+        XCTAssertEqual(wallet.status, .ready)
+    }
+
+    func test_whenStartsDeploymentInWrongState_thenThrows() throws {
+        try wallet.startDeployment()
+        try wallet.completeDeployment()
+        XCTAssertThrowsError(try wallet.startDeployment())
+    }
+
+    func test_whenCompletesDeploymentInWrongState_thenThrows() throws {
+        XCTAssertThrowsError(try wallet.completeDeployment())
+    }
+
+    func test_whenTryingToAddOwnerInPendingState_thenThrows() throws {
+        try wallet.startDeployment()
+        XCTAssertThrowsError(try wallet.addOwner(owner, kind: "kind"))
+    }
+
+    func test_whenTryingToRemoveOwnerWhenInPendingState_thenThrows() throws {
+        try wallet.addOwner(owner, kind: "kind")
+        try wallet.startDeployment()
+        XCTAssertThrowsError(try wallet.removeOwner(kind: "kind"))
+    }
+
+    func test_whenTryingToReplaceOwnerWhilePending_thenThrows() throws {
+        let otherOwner = Owner(address: BlockchainAddress(value: "Other"))
+        try wallet.addOwner(owner, kind: "kind")
+        try wallet.startDeployment()
+        XCTAssertThrowsError(try wallet.replaceOwner(with: otherOwner, kind: "kind"))
+    }
+
+    func test_whenCancellingDeployment_thenChangesState() throws {
+        try wallet.startDeployment()
+        try wallet.cancelDeployment()
+        XCTAssertEqual(wallet.status, .newDraft)
+    }
+
+    func test_whenTryingToCancelDeplolymentWhileNotDeploying_thenThrows() throws {
+        XCTAssertThrowsError(try wallet.cancelDeployment())
+        try wallet.startDeployment()
+        try wallet.completeDeployment()
+        XCTAssertThrowsError(try wallet.cancelDeployment())
     }
 
 }
