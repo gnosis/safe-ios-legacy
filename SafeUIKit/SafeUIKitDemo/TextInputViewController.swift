9818deb419afe7641befe91682334e6042b2e336
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-08 11:13:16 +0200

    WA-350 Updated

---
+++ SafeUIKit/SafeUIKitDemo/TextInputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+import SafeUIKit
+
+final class TextInputViewController: UIViewController {
+
+    @IBOutlet weak var textInput: TextInput!
+
+    @IBAction func addSuccessRule(_ sender: Any) {
+        textInput.addRule("Success Rule") { _ in true }
+    }
+
+    @IBAction func addFailingRule(_ sender: Any) {
+        textInput.addRule("Failing Rule") { _ in false }
+    }
+
+    @IBAction func addEmptyRule(_ sender: Any) {
+        textInput.addRule("Empty Rule")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textInput.accessibilityIdentifier = "testTextInput"
+    }
+
+}
