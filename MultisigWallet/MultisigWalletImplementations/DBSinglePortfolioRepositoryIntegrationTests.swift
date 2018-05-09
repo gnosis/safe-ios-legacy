a779c0c6d690794f59818040951b4fcde99ee133
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-09 15:55:01 +0200

    Added integration test for portfolio repo

---
+++ MultisigWallet/MultisigWalletImplementations/DBSinglePortfolioRepositoryIntegrationTests.swift
@@ -0,0 +1,43 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import MultisigWalletImplementations
+import MultisigWalletDomainModel
+import Database
+
+class DBSinglePortfolioRepositoryIntegrationTests: XCTestCase {
+
+    func test_all() throws {
+        let db = SQLiteDatabase(name: String(reflecting: self),
+                                fileManager: FileManager.default,
+                                sqlite: CSQLite3(),
+                                bundleId: String(reflecting: self))
+        try? db.destroy()
+        try db.create()
+        defer { try? db.destroy() }
+
+        let repo = DBSinglePortfolioRepository(db: db)
+        try repo.setUp()
+
+        let walletRepo = DBWalletRepository(db: db)
+        let wallet = Wallet(id: walletRepo.nextID())
+        let otherWallet = Wallet(id: walletRepo.nextID())
+
+        let portfolio = Portfolio(id: repo.nextID())
+        try portfolio.addWallet(wallet.id)
+        try portfolio.addWallet(otherWallet.id)
+        try portfolio.selectWallet(otherWallet.id)
+
+        try repo.save(portfolio)
+        let saved = try repo.portfolio()
+        XCTAssertEqual(saved, portfolio)
+        XCTAssertEqual(saved?.selectedWallet, portfolio.selectedWallet)
+        XCTAssertEqual(saved?.wallets, portfolio.wallets)
+
+        try repo.remove(portfolio)
+        XCTAssertNil(try repo.findByID(portfolio.id))
+    }
+
+}
