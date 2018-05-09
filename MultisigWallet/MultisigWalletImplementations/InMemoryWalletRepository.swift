775e2e038555b851c05d14e0fc258cb903d6d0d3
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-09 11:11:51 +0200

    in-memory repository

--- MultisigWallet/MultisigWalletImplementations/InMemoryWalletRepository.swift
+++ MultisigWallet/MultisigWalletImplementations/InMemoryWalletRepository.swift
@@ -7,18 +7,22 @@ import MultisigWalletDomainModel
 
 public class InMemoryWalletRepository: WalletRepository {
 
+    private var wallets = Set<Wallet>()
+
     public init() {}
 
     public func save(_ wallet: Wallet) throws {
-
+        wallets.insert(wallet)
     }
 
     public func remove(_ walletID: WalletID) throws {
-
+        if let foundWallet = try findByID(walletID) {
+            wallets.remove(foundWallet)
+        }
     }
 
     public func findByID(_ walletID: WalletID) throws -> Wallet? {
-        return nil
+        return wallets.first { $0.id == walletID }
     }
 
 }
--- MultisigWallet/MultisigWalletImplementations/InMemoryWalletRepositoryTests.swift
+++ MultisigWallet/MultisigWalletImplementations/InMemoryWalletRepositoryTests.swift
@@ -4,7 +4,28 @@
 
 import XCTest
 @testable import MultisigWalletImplementations
+import MultisigWalletDomainModel
 
 class InMemoryWalletRepositoryTests: XCTestCase {
+
+    var wallet: Wallet!
+    var repository: InMemoryWalletRepository!
+
+    override func setUp() {
+        super.setUp()
+        XCTAssertNoThrow(wallet = Wallet(id: try WalletID()))
+        repository = InMemoryWalletRepository()
+    }
+
+    func test_save_whenSaving_thenCanFindByID() throws {
+        try repository.save(wallet)
+        XCTAssertEqual(try repository.findByID(wallet.id), wallet)
+    }
+
+    func test_remove_whenRemoved_thenCannotFindIt() throws {
+        try repository.save(wallet)
+        try repository.remove(wallet.id)
+        XCTAssertNil(try repository.findByID(wallet.id))
+    }
 
 }
