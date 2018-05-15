1a6a61a7c17d0fdbf7459395ea66d6ec4020e845
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-15 15:23:48 +0200

    WA-355 added new application service

---
+++ Ethereum/EthereumApplication/EthereumApplicationService.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+
+public class EthereumApplicationService {
+
+    public init() {}
+
+    public func address(signature: String) -> String? {
+        return nil
+    }
+}
