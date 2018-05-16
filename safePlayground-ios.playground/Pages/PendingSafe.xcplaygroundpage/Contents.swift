f0d9160211c1b39747939114a9dbb4a03158ecb2
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-16 15:15:34 +0200

    WA-355 Added initial state update for pending controller

---
+++ safePlayground-ios.playground/Pages/PendingSafe.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+import PlaygroundSupport
+import SafeAppUI
+
+let controller = PendingSafeViewController.create()
+
+PlaygroundPage.current.liveView = controller
+
+//: [Next](@next)
