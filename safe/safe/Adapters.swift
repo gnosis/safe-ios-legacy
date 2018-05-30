0904ac6b2fcebf631c147564f1a842ad3824a430
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-30 17:34:29 +0200

    WA-392 gs

--- safe/safe/Adapters.swift
+++ safe/safe/Adapters.swift
@@ -8,6 +8,8 @@ import EthereumApplication
 
 extension EthereumApplicationService: BlockchainDomainService {
 
+    static let pollingInterval: TimeInterval = 5
+
     public func requestWalletCreationData(owners: [String], confirmationCount: Int) throws -> WalletCreationData {
         let data = try createSafeCreationTransaction(owners: owners, confirmationCount: confirmationCount)
         return WalletCreationData(walletAddress: data.safe, fee: data.payment)
@@ -18,16 +20,24 @@ extension EthereumApplicationService: BlockchainDomainService {
     }
 
     public func observeBalance(account: String, observer: @escaping BlockchainBalanceObserver) {
-        observeBalance(address: account, every: 5) { newBalance -> Bool in
-            let response = observer(account, newBalance)
-            return response == .stopObserving
+        do {
+            try observeBalance(address: account, every: EthereumApplicationService.pollingInterval) { newBalance in
+                let response = observer(account, newBalance)
+                return response == .stopObserving
+            }
+        } catch let error {
+            ApplicationServiceRegistry.logger.fatal("Failed to observe balance", error: error)
         }
     }
 
     public func createWallet(address: String, completion: @escaping (Bool, Error?) -> Void) {
         do {
-            _ = try startSafeCreation(address: address)
-            completion(true, nil)
+            let txHash = try startSafeCreation(address: address)
+            try observeTransaction(hash: txHash, every: EthereumApplicationService.pollingInterval) { status in
+                if let status = status {
+                    completion(status, nil)
+                }
+            }
         } catch let error {
             completion(false, error)
         }
