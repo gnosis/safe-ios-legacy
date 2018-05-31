c9f94a26bdfda9b06f30fd71ebab2a4d5ef9efad
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-05-31 16:14:02 +0200

    WA-393 Added balance showing on main screen

--- SafeUIKit/SafeAppUI/AppFlow/MainViewController.swift
+++ SafeUIKit/SafeAppUI/AppFlow/MainViewController.swift
@@ -3,16 +3,34 @@
 //
 
 import UIKit
+import MultisigWalletApplication
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var addressFieldLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var balanceFieldLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+
+    struct Strings {
+        static let addressLabel = LocalizedString("main.label.address", comment: "Address label")
+        static let balanceLabel = LocalizedString("main.label.balance", comment: "Balance label")
+    }
+
     static func create() -> MainViewController {
-        return MainViewController()
+        return StoryboardScene.AppFlow.mainViewController.instantiate()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        let service = ApplicationServiceRegistry.walletService
+        if let balance = service.accountBalance(token: "ETH") {
+            balanceLabel.text = "\(balance) Wei"
+        }
+        if let address = service.selectedWalletAddress {
+            addressLabel.text = address
+        }
     }
 
 }
