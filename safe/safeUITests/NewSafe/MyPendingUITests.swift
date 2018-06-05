27ddfb3be9ba762f4fb3b763458f6f11bc42fa28
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-06-05 10:30:36 +0200

    WA-396 Added abort test

--- safe/safeUITests/NewSafe/MyPendingUITests.swift
+++ safe/safeUITests/NewSafe/MyPendingUITests.swift
@@ -9,6 +9,7 @@ class MyPendingSafeScreenUITests: UITestCase {
 
     let pendingScreen = PendingSafeScreen()
     let mainScreen = MainScreen()
+    let newSafeScreen = NewSafeScreen()
 
     class PendingSafeScreen {
         let title = XCUIApplication().staticTexts[LocalizedString("pending_safe.title")]
@@ -30,10 +31,7 @@ class MyPendingSafeScreenUITests: UITestCase {
         application.setMockServerResponseDelay(1)
         givenDeploymentStarted()
         waitUntil(pendingScreen.title, .exists)
-        let requiredEth = "100 Wei"
-        let balanceEth = "0 Wei"
-        let status = String(format: LocalizedString("pending_safe.status.not_enough_funds"), balanceEth, requiredEth)
-        waitUntil(pendingScreen.status.label == status, timeout: 10)
+        waitUntilNotEnoughFundsStatus()
         XCTAssertEqual(pendingScreen.progressView.value as? String, "20%")
     }
 
@@ -42,23 +40,59 @@ class MyPendingSafeScreenUITests: UITestCase {
         application.setMockServerResponseDelay(2)
         givenDeploymentStarted()
         waitUntil(pendingScreen.title, .exists)
-        let alertExpectation = expectation(description: "abort alert")
-        let alertSubscription = addUIInterruptionMonitor(withDescription: "Abort") { [unowned self] alert in
-            defer { alertExpectation.fulfill() }
-            guard alert.title == LocalizedString("pending_safe.abort_alert.title") else { return false }
-            XCTAssertEqual(alert.title, LocalizedString("pending_safe.abort_alert.message"))
-            XCTAssertExist(alert.buttons[LocalizedString("pending_safe.abort_alert.abort")])
-            let cancelButton = alert.buttons[LocalizedString("pending_safe.abort_alert.cancel")]
-            XCTAssertExist(cancelButton)
-            cancelButton.tap()
-            return true
-        }
-        defer { removeUIInterruptionMonitor(alertSubscription) }
+        let (alertMonitor, alertExpectation) = addAbortAlertMonitor(cancellingAlert: true)
+        defer { removeUIInterruptionMonitor(alertMonitor) }
         pendingScreen.cancel.tap()
-        delay(1)
-        XCUIApplication().tap()
-        waitForExpectations(timeout: 1)
+        handleAlert()
         waitUntil(mainScreen.addressLabel, .exists)
     }
 
+    // NS-202
+    func test_whenAbortsDeployment_thenGoesBack() {
+        application.setMockServerResponseDelay(1)
+        givenDeploymentStarted()
+        waitUntil(pendingScreen.title, .exists)
+        waitUntilNotEnoughFundsStatus()
+        let (monitor, expectation) = addAbortAlertMonitor(cancellingAlert: false)
+        defer { removeUIInterruptionMonitor(monitor) }
+        pendingScreen.cancel.tap()
+        handleAlert()
+        waitUntil(newSafeScreen.isDisplayed)
+    }
+
+}
+
+extension MyPendingSafeScreenUITests {
+
+    private func handleAlert() {
+        delay(1)
+        XCUIApplication().tap()
+        waitForExpectations(timeout: 1)
+    }
+
+    private func waitUntilNotEnoughFundsStatus() {
+        let requiredEth = "100 Wei"
+        let balanceEth = "0 Wei"
+        let status = String(format: LocalizedString("pending_safe.status.not_enough_funds"), balanceEth, requiredEth)
+        waitUntil(pendingScreen.status.label == status, timeout: 10)
+    }
+
+    private func addAbortAlertMonitor(cancellingAlert: Bool) -> (NSObjectProtocol, XCTestExpectation) {
+        let alertExpectation = expectation(description: "abort alert")
+        let alertMonitor = addUIInterruptionMonitor(withDescription: "Abort") { [unowned self] alert in
+            defer { alertExpectation.fulfill() }
+            guard alert.staticTexts[LocalizedString("pending_safe.abort_alert.title")].exists else { return false }
+            let abortButton = alert.buttons[LocalizedString("pending_safe.abort_alert.abort")]
+            XCTAssertExist(abortButton)
+            let cancelButton = alert.buttons[LocalizedString("pending_safe.abort_alert.cancel")]
+            XCTAssertExist(cancelButton)
+            if cancellingAlert {
+                cancelButton.tap()
+            } else {
+                abortButton.tap()
+            }
+            return true
+        }
+        return (alertMonitor, alertExpectation)
+    }
 }
