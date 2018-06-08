777e461b46c92b08a16a5e9a1d321b3d136f6bf8
Andrey Scherbovich <andrey@gnosis.pm>
2018-06-08 16:22:35 +0200

    WA-417 Handling of typing events in TokenInput

--- SafeUIKit/SafeUIKit/TokenInput.swift
+++ SafeUIKit/SafeUIKit/TokenInput.swift
@@ -8,8 +8,14 @@ public class TokenInput: UIView {
 
     @IBOutlet weak var integerPartTextField: UITextField!
     @IBOutlet weak var fractionalPartTextField: UITextField!
-    @IBOutlet weak var tokenImageView: UIImageView!
     @IBOutlet weak var currencyValueLabel: UILabel!
+
+    private let delimiter: Character = "."
+
+    enum Field: Int {
+        case integer
+        case fractional
+    }
 
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -26,6 +32,46 @@ public class TokenInput: UIView {
 
     private func configure() {
         safeUIKit_loadFromNib()
+        integerPartTextField.delegate = self
+        integerPartTextField.tag = Field.integer.rawValue
+        fractionalPartTextField.delegate = self
+        fractionalPartTextField.tag = Field.fractional.rawValue
+    }
+
+    public override var isFirstResponder: Bool {
+        return integerPartTextField.isFirstResponder || fractionalPartTextField.isFirstResponder
+    }
+
+    public override func resignFirstResponder() -> Bool {
+        integerPartTextField.resignFirstResponder()
+        fractionalPartTextField.resignFirstResponder()
+        return super.resignFirstResponder()
+    }
+
+}
+
+extension TokenInput: UITextFieldDelegate {
+
+    public func textField(_ textField: UITextField,
+                          shouldChangeCharactersIn range: NSRange,
+                          replacementString string: String) -> Bool {
+        print("Range: \(range)")
+        print("Replacement string: \(string)")
+        return true
+    }
+
+    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.text = textField.text?.replacingOccurrences(of: String(delimiter), with: "")
+        return true
+    }
+
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        if textField.tag == Field.integer.rawValue &&
+            !text.isEmpty &&
+            text.last != delimiter {
+            textField.text! += String(delimiter)
+        }
     }
 
 }
--- SafeUIKit/SafeUIKitDemo/TokenInputViewController.swift
+++ SafeUIKit/SafeUIKitDemo/TokenInputViewController.swift
@@ -3,13 +3,20 @@
 //
 
 import UIKit
+import SafeUIKit
 
 class TokenInputViewController: UIViewController {
 
-    @IBOutlet var tokenInput: UIView!
-    
+    @IBOutlet var tokenInput: TokenInput!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func hideKeyboard() {        
+        _ = tokenInput.resignFirstResponder()
     }
 
 }
