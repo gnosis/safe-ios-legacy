2294d5c962c926b5f302c6cbfd14c999580641bf
Andrey Scherbovich <andrey@gnosis.pm>
2018-06-13 10:02:23 +0200

    WA-417 more tests

--- SafeUIKit/SafeUIKit/ApproximateCurrencyFormatterTests.swift
+++ SafeUIKit/SafeUIKit/ApproximateCurrencyFormatterTests.swift
@@ -10,7 +10,7 @@ class ApproximateCurrencyFormatterTests: XCTestCase {
 
     let approxFormatter = ApproximateCurrencyFormatter(locale: Locale(identifier: "de_DE"))
 
-    func test_formatting() {
+    func test_formattingFromBigInt() {
         assertFormatting(BigInt(0), 3, "0,00")
         assertFormatting(BigInt(5), 3, "0,00")
         assertFormatting(BigInt(6), 3, "0,01")
@@ -24,8 +24,26 @@ class ApproximateCurrencyFormatterTests: XCTestCase {
         assertFormatting(BigInt(100_000_000_000_006), 3, "")
     }
 
+    func test_formattingFromDouble() {
+        assertFormatting(0, "0,00")
+        assertFormatting(0.005, "0,00")
+        assertFormatting(0.006, "0,01")
+        assertFormatting(1, "1,00")
+        assertFormatting(1.01, "1,01")
+        assertFormatting(1.11, "1,11")
+        assertFormatting(1.001, "1,00")
+        assertFormatting(1.005, "1,00")
+        assertFormatting(1.006, "1,01")
+        assertFormatting(10_000_000_000.006, "10.000.000.000,01")
+        assertFormatting(100_000_000_000.006, "")
+    }
+
     private func assertFormatting(_ value: BigInt, _ decimals: Int, _ expected: String) {
         XCTAssertEqual(approxFormatter.string(from: value, decimals: decimals), expected == "" ? "" : "≈ \(expected) €")
+    }
+
+    private func assertFormatting(_ value: Double, _ expected: String) {
+        XCTAssertEqual(approxFormatter.string(from: value), expected == "" ? "" : "≈ \(expected) €")
     }
 
 }
