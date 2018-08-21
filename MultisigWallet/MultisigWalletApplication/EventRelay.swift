05ab2871974af0df605fa18348a7a3f06581942f
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-08-21 15:16:30 +0200

    WA-525 Added event relay class

---
+++ MultisigWallet/MultisigWalletApplication/EventRelay.swift
@@ -0,0 +1,50 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+import MultisigWalletDomainModel
+
+public protocol EventSubscriber: class {
+    func notify()
+}
+
+/// Relays published events from EventPublisher to EventSubscribers.
+/// When event subscriber is deallocated, it won't be notified again, therefore unsubscribing is optional.
+class EventRelay {
+
+    private class SubscriberWrapper {
+        weak var ref: EventSubscriber?
+
+        init(_ ref: EventSubscriber) {
+            self.ref = ref
+        }
+    }
+
+    private var subscribedToPublisher = false
+    private var subscribers = [(type: DomainEvent.Type, subscriber: SubscriberWrapper)]()
+
+    func subscribe(_ subject: EventSubscriber, for event: DomainEvent.Type) {
+        if !subscribedToPublisher {
+            DomainRegistry.eventPublisher.subscribe(handleEvent)
+            subscribedToPublisher = true
+        }
+        subscribers.append((event, SubscriberWrapper(subject)))
+    }
+
+    func unsubscribe(_ subject: EventSubscriber) {
+        if let index = subscribers.index(where: { $0.subscriber.ref === subject }) {
+            subscribers.remove(at: index)
+        }
+    }
+
+    private func handleEvent(_ event: DomainEvent) {
+        subscribers = subscribers.filter { $0.subscriber.ref != nil }
+        subscribers.filter {
+            $0.type == type(of: event) || $0.type == DomainEvent.self
+        }.forEach {
+            $0.subscriber.ref!.notify()
+        }
+    }
+
+}
---
+++ MultisigWallet/MultisigWalletApplication/EventRelayTests.swift
@@ -0,0 +1,93 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import MultisigWalletApplication
+import MultisigWalletDomainModel
+
+class EventRelayTests: XCTestCase {
+
+    let relay = EventRelay()
+    let subscriber = MockSubscriber()
+    let publisher = EventPublisher()
+
+    override func setUp() {
+        super.setUp()
+        DomainRegistry.put(service: publisher, for: EventPublisher.self)
+    }
+
+    func test_api() {
+        relay.subscribe(subscriber, for: MyEvent.self)
+        subscriber.expect_notify()
+        publisher.publish(MyEvent())
+        subscriber.verify()
+    }
+
+    func test_whenSubscriberDeallocated_thenItIsNotNotified() {
+        var callCount = 0
+        var temp: BlockSubscriber? = BlockSubscriber {
+            callCount += 1
+        }
+        relay.subscribe(temp!, for: DomainEvent.self)
+        publisher.publish(MyEvent())
+        temp = nil
+        publisher.publish(MyEvent())
+        XCTAssertEqual(callCount, 1)
+    }
+
+    func test_whenSubscribing_thenDoes() {
+        let other = MockSubscriber()
+        subscriber.expect_notify()
+        other.expect_notify()
+        relay.subscribe(subscriber, for: DomainEvent.self)
+        relay.subscribe(other, for: DomainEvent.self)
+        publisher.publish(MyEvent())
+        subscriber.verify()
+        other.verify()
+    }
+
+    func test_whenUnsubscribed_thenIsNotNotified() {
+        relay.subscribe(subscriber, for: DomainEvent.self)
+        relay.unsubscribe(subscriber)
+        publisher.publish(MyEvent())
+        subscriber.verify()
+    }
+
+}
+
+class BlockSubscriber: EventSubscriber {
+
+    var block: () -> Void
+
+    init(_ block: @escaping () -> Void) {
+        self.block = block
+    }
+
+    func notify() {
+        block()
+    }
+
+}
+
+class MockSubscriber: EventSubscriber {
+
+    private var expected_notify = [String]()
+    private var actual_notify = [String]()
+
+    func expect_notify() {
+        expected_notify.append("notify()")
+    }
+
+    func notify() {
+        actual_notify.append(#function)
+    }
+
+    func verify(file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(actual_notify, expected_notify, file: file, line: line)
+    }
+}
+
+class MyEvent: DomainEvent {
+
+}
