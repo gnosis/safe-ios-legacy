e694c7d32b9f870101e33bdd8f9c8f162cf786b2
Andrey Scherbovich <andrey@gnosis.pm>
2018-10-03 14:21:12 +0200

    WA-539 unit tests

--- SafeUIKit/SafeAppUI/AppFlow/MainFlow/SafeAddress/SafeAddressViewControllerTests.swift
+++ SafeUIKit/SafeAppUI/AppFlow/MainFlow/SafeAddress/SafeAddressViewControllerTests.swift
@@ -4,11 +4,27 @@
 
 import XCTest
 @testable import SafeAppUI
+import MultisigWalletApplication
+import CommonTestSupport
 
 class SafeAddressViewControllerTests: XCTestCase {
 
-    func test_canCreate() {
-        XCTAssertNotNil(SafeAddressViewController())
+    let walletService = MockWalletApplicationService()
+    let testAddress = "test_address"
+
+    override func setUp() {
+        super.setUp()
+        ApplicationServiceRegistry.put(service: walletService, for: WalletApplicationService.self)
+        walletService.assignAddress(testAddress)
+    }
+
+    func test_whenCreated_thenDisplaysCorrectData() {
+        let controller = SafeAddressViewController.create()
+        createWindow(controller)
+        controller.viewDidLoad()
+        XCTAssertEqual(controller.safeAddressLabel.text, testAddress)
+        XCTAssertEqual(controller.qrCodeView.value, testAddress)
+        XCTAssertEqual(controller.identiconView.seed, testAddress)
     }
 
 }
