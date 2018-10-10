ef43bb472f6d18d7b256625a10fbb06908864e1d
Andrey Scherbovich <andrey@gnosis.pm>
2018-10-10 10:20:37 +0200

    GHI-275 add unit tests

--- SafeUIKit/SafeUIKit/Components/TextInputs/TextInput/TextInput1Tests.swift
+++ SafeUIKit/SafeUIKit/Components/TextInputs/TextInput/TextInput1Tests.swift
@@ -7,11 +7,29 @@ import XCTest
 
 class TextInput1Tests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
+    let textInput = TextInput1()
+
+    func test_whenInitialized_thenDefaultValuesAreCorrect() {
+        XCTAssertEqual(textInput.placeholder, nil)
+        XCTAssertEqual(textInput.isDimmed, false)
+        XCTAssertEqual(textInput.leftImage, nil)
+        XCTAssertEqual(textInput.font, UIFont.systemFont(ofSize: 17))
+        XCTAssertEqual(textInput.layer.borderColor, UIColor.white.cgColor)
+        XCTAssertEqual(textInput.layer.cornerRadius, 6)
+        XCTAssertEqual(textInput.layer.borderWidth, 1)
+        XCTAssertTrue(textInput.rightView is UIButton)
     }
 
-    func test_whenCondition_thenResult() {
+    func test_whenLeftImageIsSet_thenLeftViewIsDisplayed() {
+        XCTAssertNil(textInput.leftView)
+        textInput.leftImage = UIImage()
+        XCTAssertNotNil(textInput.leftView)
+    }
+
+    func test_whenPlaceholderIsSet_thenDiplaysIt() {
+        XCTAssertNil(textInput.attributedPlaceholder)
+        textInput.placeholder = "placeholder"
+        XCTAssertNotNil(textInput.attributedPlaceholder)
     }
 
 }
