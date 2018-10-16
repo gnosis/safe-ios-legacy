f6f4ba2679bbb7a477b893a19f7e711223db7233
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-10-16 10:29:05 +0200

    GHI-292  Removed payment adjustment

--- MultisigWallet/MultisigWalletDomainModel/TransactionRelayService/RelayRequests/SafeCreationTransactionRequest.swift
+++ MultisigWallet/MultisigWalletDomainModel/TransactionRelayService/RelayRequests/SafeCreationTransactionRequest.swift
@@ -60,7 +60,7 @@ public struct SafeCreationTransactionRequest: Encodable {
         }
 
         public var deploymentFee: BigInt {
-            return BigInt(payment)! + 9 // FIXME: server-side bug with rounding of payment value
+            return BigInt(payment)!
         }
 
         public init(signature: Response.Signature,
