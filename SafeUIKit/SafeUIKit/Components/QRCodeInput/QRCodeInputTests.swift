b88f5fca03dfd3deac1316e1b06d6e1e1b531891
Andrey Scherbovich <andrey@gnosis.pm>
2018-10-17 17:38:24 +0200

    GHI-287 try to increase delay for Travis

--- SafeUIKit/SafeUIKit/Components/QRCodeInput/QRCodeInputTests.swift
+++ SafeUIKit/SafeUIKit/Components/QRCodeInput/QRCodeInputTests.swift
@@ -32,14 +32,14 @@ class QRCodeInputTests: XCTestCase {
     func test_textFieldShouldBeginEditing_whenScanOnly_thenCallsDelegateAndReturnsFalse() {
         barcodeTextField.editingMode = .scanOnly
         XCTAssertFalse(barcodeTextField.textFieldShouldBeginEditing(barcodeTextField))
-        delay()
+        delay(0.25)
         assertScannerPresented()
     }
 
     func test_openBarcodeSacenner_callsDelegate() {
         let button = barcodeTextField.rightView as! UIButton
         button.sendActions(for: .touchUpInside)
-        delay()
+        delay(0.25)
         assertScannerPresented()
     }
 
