f69bff1edb118c5d1561842b13d51cd2d5cfcae5
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-10-26 15:54:23 +0200

    GHI-324 Fixed tests

--- SafeUIKit/SafeAppUI/OnboardingFlow/OnboardingFlowCoordinatorTests.swift
+++ SafeUIKit/SafeAppUI/OnboardingFlow/OnboardingFlowCoordinatorTests.swift
@@ -23,20 +23,6 @@ class OnboardingFlowCoordinatorTests: SafeTestCase {
         XCTAssertTrue(flowCoordinator.navigationController.topViewController is StartViewController)
     }
 
-    func test_startViewController_whenNoMasterPassword_thenMasterPasswordFlowStarted() {
-        let testFC = TestFlowCoordinator()
-        let masterPasswordFC = MasterPasswordFlowCoordinator()
-        testFC.enter(flow: masterPasswordFC)
-        let expectedController = testFC.topViewController
-
-        authenticationService.unregisterUser()
-        flowCoordinator.setUp()
-        flowCoordinator.didStart()
-
-        XCTAssertNotNil(flowCoordinator.navigationController.topViewController)
-        XCTAssertTrue(type(of: flowCoordinator.navigationController.topViewController) == type(of: expectedController))
-    }
-
     func test_whenStartingSetupPassword_thenShowsTermsScreen() {
         pressSetupPasswordButton()
         XCTAssertTrue(flowCoordinator.rootViewController.presentedViewController is TermsAndConditionsViewController)
@@ -79,7 +65,7 @@ class OnboardingFlowCoordinatorTests: SafeTestCase {
     func test_whenOpensPrivacyPolicy_thenOpensSafari() {
         let service = MockWalletApplicationService()
         ApplicationServiceRegistry.put(service: service, for: WalletApplicationService.self)
-        service.termsOfUseURL = URL(string: "https://gnosis.pm/")!
+        service.privacyPolicyURL = URL(string: "https://gnosis.pm/")!
         pressSetupPasswordButton()
         flowCoordinator.wantsToOpenPrivacyPolicy()
         delay(1.0)
@@ -101,16 +87,16 @@ class OnboardingFlowCoordinatorTests: SafeTestCase {
     }
 
     func test_whenDidConfirmPassword_thenSetupSafeIsShown() {
-        authenticationService.unregisterUser()
-        walletService.expect_isWalletDeployable(false)
-        flowCoordinator.setUp()
-        flowCoordinator.didStart()
-        delay(0.25)
+        let service = MockWalletApplicationService()
+        ApplicationServiceRegistry.put(service: service, for: WalletApplicationService.self)
+        service.expect_isWalletDeployable(false)
+        service.expect_isSafeCreationInProgress(false)
+        pressSetupPasswordButton()
+        flowCoordinator.didAgree()
+        delay(1.25)
         flowCoordinator.masterPasswordFlowCoordinator.didConfirmPassword()
         delay(0.25)
-        let controller = flowCoordinator.navigationController.topViewController
-        print(String(reflecting: controller.self))
-        XCTAssertTrue(controller is SetupSafeOptionsViewController)
+        XCTAssertTrue(flowCoordinator.navigationController.topViewController is SetupSafeOptionsViewController)
     }
 
     func test_whenSetupSafeFlowExits_thenOnboardingFlowExits() {
