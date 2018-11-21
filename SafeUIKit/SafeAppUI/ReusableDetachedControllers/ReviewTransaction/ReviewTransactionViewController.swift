2a4b636f3f6742bf9aca3c01ab1bb2ca5382524e
Andrey Scherbovich <andrey@gnosis.pm>
2018-11-21 16:52:37 +0100

    GHI-352 add transfer view and transaction fee cells

--- SafeUIKit/SafeAppUI/ReusableDetachedControllers/ReviewTransaction/ReviewTransactionViewController.swift
+++ SafeUIKit/SafeAppUI/ReusableDetachedControllers/ReviewTransaction/ReviewTransactionViewController.swift
@@ -5,10 +5,13 @@
 import UIKit
 import SafeUIKit
 import MultisigWalletApplication
+import Common
+import BigInt
 
 final class ReviewTransactionViewController: UITableViewController {
 
     private var tx: TransactionData!
+    private var cells = [IndexPath: UITableViewCell]()
 
     enum Strings {
         static let outgoingTransfer = LocalizedString("transaction.outgoing_transfer", comment: "Outgoing transafer")
@@ -22,10 +25,13 @@ final class ReviewTransactionViewController: UITableViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        dequeueCellsOnce()
     }
 
     private func configureTableView() {
         tableView.register(TransactionHeaderCell.self, forCellReuseIdentifier: "TransactionHeaderCell")
+        tableView.register(TransferViewCell.self, forCellReuseIdentifier: "TransferViewCell")
+        tableView.register(TransactionFeeCell.self, forCellReuseIdentifier: "TransactionFeeCell")
         tableView.separatorStyle = .none
         tableView.rowHeight = UITableView.automaticDimension
         tableView.backgroundView = BackgroundImageView(frame: tableView.frame)
@@ -33,22 +39,86 @@ final class ReviewTransactionViewController: UITableViewController {
         tableView.tableFooterView = UIView()
     }
 
-}
+    private func dequeueCellsOnce() {
+        addHeaderCell(for: IndexPath(row: 0, section: 0))
+        addTransferViewCell(for: IndexPath(row: 1, section: 0))
+        addConfirmationCell(for: IndexPath(row: 2, section: 0))
+        if tx.amountTokenData.isEther {
+            addTransactionFeeCellForEther(for: IndexPath(row: 3, section: 0))
+        } else {
+            addTransactionFeeCellForTokenTransfer_inToken(for: IndexPath(row: 3, section: 0))
+            addTransactionFeeCellForTokenTransfer_inEther(for: IndexPath(row: 4, section: 0))
+        }
+    }
 
-// MARK: - Table view data source
+    private func addHeaderCell(for indexPath: IndexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionHeaderCell",
+                                                 for: indexPath) as! TransactionHeaderCell
+        cell.configure(imageURL: tx.amountTokenData.logoURL,
+                       code: tx.amountTokenData.code,
+                       info: Strings.outgoingTransfer)
+        cells[indexPath] = cell
+    }
 
-extension ReviewTransactionViewController {
+    private func addTransferViewCell(for indexPath: IndexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransferViewCell",
+                                                 for: indexPath) as! TransferViewCell
+        cell.transferView.fromAddress = tx.sender
+        cell.transferView.toAddress = tx.recipient
+        cell.transferView.tokenData = tx.amountTokenData
+        cells[indexPath] = cell
+    }
+
+    private func addConfirmationCell(for indexPath: IndexPath) {
+        cells[indexPath] = UITableViewCell()
+    }
+
+    private func addTransactionFeeCellForEther(for indexPath: IndexPath) {
+        let (cell, currentBalance) = cellAndCurrentBalance(for: indexPath)
+        let currentBalanceTokenData = tx.amountTokenData.copy(balance: currentBalance)
+        let resultingBalance =
+            tx.amountTokenData.copy(balance: currentBalance - tx.amountTokenData.balance! - tx.feeTokenData.balance!)
+        cell.transactionFeeView.configure(currentBalance: currentBalanceTokenData,
+                                          transactionFee: tx.feeTokenData,
+                                          resultingBalance: resultingBalance)
+        cells[indexPath] = cell
+    }
+
+    private func addTransactionFeeCellForTokenTransfer_inToken(for indexPath: IndexPath) {
+        let (cell, currentBalance) = cellAndCurrentBalance(for: indexPath)
+        let currentBalanceTokenData = tx.amountTokenData.copy(balance: currentBalance)
+        let resultingBalance = tx.amountTokenData.copy(balance: currentBalance - tx.amountTokenData.balance!)
+        cell.transactionFeeView.configure(currentBalance: currentBalanceTokenData,
+                                          transactionFee: nil,
+                                          resultingBalance: resultingBalance)
+        cells[indexPath] = cell
+    }
+
+    private func addTransactionFeeCellForTokenTransfer_inEther(for indexPath: IndexPath) {
+        let (cell, currentBalance) = cellAndCurrentBalance(for: indexPath)
+        let resultingBalance = tx.amountTokenData.copy(balance: currentBalance - tx.feeTokenData.balance!)
+        cell.transactionFeeView.configure(currentBalance: nil,
+                                          transactionFee: tx.feeTokenData,
+                                          resultingBalance: resultingBalance)
+        cells[indexPath] = cell
+    }
+
+    private func cellAndCurrentBalance(for indexPath: IndexPath) -> (TransactionFeeCell, BigInt) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionFeeCell",
+                                                 for: indexPath) as! TransactionFeeCell
+        let tokenID = BaseID(tx.amountTokenData.address)
+        let currentBalance = ApplicationServiceRegistry.walletService.accountBalance(tokenID: tokenID)!
+        return (cell, currentBalance)
+    }
+
+    // MARK: - table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return cells.keys.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionHeaderCell",
-                                                 for: indexPath) as! TransactionHeaderCell
-        let imageURL: URL? = tx.tokenLogoUrl.isEmpty ? nil : URL(string: tx.tokenLogoUrl)!
-        cell.configure(imageURL: imageURL, code: tx.token, info: Strings.outgoingTransfer)
-        return cell
+        return cells[indexPath]!
     }
 
 }
--- SafeUIKit/SafeAppUI/ReusableDetachedControllers/ReviewTransaction/ReviewTransactionViewControllerTests.swift
+++ SafeUIKit/SafeAppUI/ReusableDetachedControllers/ReviewTransaction/ReviewTransactionViewControllerTests.swift
@@ -19,14 +19,56 @@ class ReviewTransactionViewControllerTests: XCTestCase {
     }
 
     func test_whenLoaded_thenSetsTransactionHeaderAccordingToTransactionData() {
-        let data = TransactionData.create1(status: .readyToSubmit)
-        service.transactionData_output = data
-        let vc = ReviewTransactionViewController(transactionID: data.id)
-        vc.loadViewIfNeeded()
+        let (data, vc) = ethDataAndCotroller()
         let headerCell = vc.cellForRow(0) as! TransactionHeaderCell
-        XCTAssertEqual(headerCell.transactionHeaderView.assetCode, data.token)
+        XCTAssertEqual(headerCell.transactionHeaderView.assetCode, data.amountTokenData.code)
         XCTAssertEqual(headerCell.transactionHeaderView.assetInfo,
                        LocalizedString("transaction.outgoing_transfer", comment: ""))
+    }
+
+    func test_whenLoaded_thenSetsTransferViewAccordingToTransactionData() {
+        let (data, vc) = ethDataAndCotroller()
+        let transferViewCell = vc.cellForRow(1) as! TransferViewCell
+        XCTAssertEqual(transferViewCell.transferView.fromAddress, data.sender)
+        XCTAssertEqual(transferViewCell.transferView.toAddress, data.recipient)
+        XCTAssertEqual(transferViewCell.transferView.tokenData, data.amountTokenData)
+    }
+
+    func test_whenLoadedForEtherTransfer_thenHasOneTransactionFeeCell() {
+        let (_, vc) = ethDataAndCotroller()
+        XCTAssertTrue(vc.cellForRow(3) is TransactionFeeCell)
+        XCTAssertEqual(vc.cellsCount(), 4)
+    }
+
+    func test_whenLoadedForTokenTransfer_thenHasTwoTransactionFeeCells() {
+        let (_, vc) = tokenDataAndCotroller()
+        XCTAssertTrue(vc.cellForRow(3) is TransactionFeeCell)
+        XCTAssertTrue(vc.cellForRow(4) is TransactionFeeCell)
+        XCTAssertEqual(vc.cellsCount(), 5)
+    }
+
+}
+
+private extension ReviewTransactionViewControllerTests {
+
+    func ethDataAndCotroller() -> (TransactionData, ReviewTransactionViewController) {
+        let data = TransactionData.ethData(status: .readyToSubmit)
+        let vc = controller(for: data)
+        return (data, vc)
+    }
+
+    func tokenDataAndCotroller() -> (TransactionData, ReviewTransactionViewController) {
+        let data = TransactionData.tokenData(status: .readyToSubmit)
+        let vc = controller(for: data)
+        return (data, vc)
+    }
+
+    func controller(for data: TransactionData) -> ReviewTransactionViewController {
+        service.transactionData_output = data
+        service.update(account: BaseID(data.amountTokenData.address), newBalance: BigInt(10).power(19))
+        let vc = ReviewTransactionViewController(transactionID: data.id)
+        vc.viewDidLoad()
+        return vc
     }
 
 }
@@ -34,25 +76,38 @@ class ReviewTransactionViewControllerTests: XCTestCase {
 private extension ReviewTransactionViewController {
 
     func cellForRow(_ row: Int) -> UITableViewCell {
-        return tableView.cellForRow(at: IndexPath(row: row, section: 0))!
+        return tableView(tableView, cellForRowAt: IndexPath(row: row, section: 0))
+    }
+
+    func cellsCount() -> Int {
+        return tableView(tableView, numberOfRowsInSection: 0)
     }
 
 }
 
 extension TransactionData {
 
-    // TODO: rename when finish refactoring
-    static func create1(status: Status) -> TransactionData {
+    static func ethData(status: Status) -> TransactionData {
         return TransactionData(id: "some",
                                sender: "some",
                                recipient: "some",
-                               amount: 100,
-                               token: "ETH",
-                               tokenDecimals: 18,
-                               tokenLogoUrl: "",
-                               fee: BigInt(10).power(18),
-                               feeToken: "ETH",
-                               feeTokenDecimals: 18,
+                               amountTokenData: TokenData.Ether.copy(balance: BigInt(10).power(18)),
+                               feeTokenData: TokenData.Ether.copy(balance: BigInt(10).power(17)),
+                               status: status,
+                               type: .outgoing,
+                               created: nil,
+                               updated: nil,
+                               submitted: nil,
+                               rejected: nil,
+                               processed: nil)
+    }
+
+    static func tokenData(status: Status) -> TransactionData {
+        return TransactionData(id: "some",
+                               sender: "some",
+                               recipient: "some",
+                               amountTokenData: TokenData.gno.copy(balance: BigInt(10).power(18)),
+                               feeTokenData: TokenData.gno.copy(balance: BigInt(10).power(17)),
                                status: status,
                                type: .outgoing,
                                created: nil,
