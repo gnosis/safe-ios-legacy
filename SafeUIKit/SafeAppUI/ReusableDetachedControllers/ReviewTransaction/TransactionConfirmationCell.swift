1fc8eb37a04732b7c3dc8a008417fd54ce058318
Andrey Scherbovich <andrey@gnosis.pm>
2018-11-23 15:00:53 +0100

    GHI-352 added logic to show and hide confirmation view

---
+++ SafeUIKit/SafeAppUI/ReusableDetachedControllers/ReviewTransaction/TransactionConfirmationCell.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TransactionConfirmationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
