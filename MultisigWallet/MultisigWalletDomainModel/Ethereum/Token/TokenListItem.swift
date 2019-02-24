a707badf78a87b1bc77a5f6365ecd79888496f4c
Valeriy Van <github@w7software.com>
2019-02-24 12:17:14 +0100

    Moves Decodable conformance into extansion

--- MultisigWallet/MultisigWalletDomainModel/Ethereum/Token/TokenListItem.swift
+++ MultisigWallet/MultisigWalletDomainModel/Ethereum/Token/TokenListItem.swift
@@ -5,7 +5,7 @@
 import Foundation
 import Common
 
-public final class TokenListItem: IdentifiableEntity<TokenID>, Decodable {
+public final class TokenListItem: IdentifiableEntity<TokenID> {
 
     public let token: Token
     public private(set) var status: TokenListItemStatus
@@ -16,29 +16,6 @@ public final class TokenListItem: IdentifiableEntity<TokenID>, Decodable {
         case regular
         case whitelisted
         case blacklisted
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case `default`
-        case code = "symbol"
-        case name
-        case decimals
-        case address
-        case logoUrl = "logoUri"
-    }
-
-    public convenience init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        let code = try values.decode(String.self, forKey: .code)
-        let name = try values.decode(String.self, forKey: .name)
-        let decimals = try values.decode(Int.self, forKey: .decimals)
-        let addressValue = try values.decode(String.self, forKey: .address)
-        let address = Address(addressValue)
-        let logoUrl = try values.decode(String.self, forKey: .logoUrl)
-        let `default` = try values.decode(Bool.self, forKey: .default)
-        let token = Token(code: code, name: name, decimals: decimals, address: address, logoUrl: logoUrl)
-        let status: TokenListItemStatus = `default` ? .whitelisted : .regular
-        self.init(token: token, status: status)
     }
 
     public init(token: Token, status: TokenListItemStatus, sortingId: Int? = nil, updated: Date = Date()) {
@@ -63,7 +40,34 @@ public final class TokenListItem: IdentifiableEntity<TokenID>, Decodable {
 
 }
 
-public struct TokenList: Decodable {
+extension TokenListItem: Decodable {
+
+    enum CodingKeys: String, CodingKey {
+        case `default`
+        case code = "symbol"
+        case name
+        case decimals
+        case address
+        case logoUrl = "logoUri"
+    }
+
+    public convenience init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let code = try values.decode(String.self, forKey: .code)
+        let name = try values.decode(String.self, forKey: .name)
+        let decimals = try values.decode(Int.self, forKey: .decimals)
+        let addressValue = try values.decode(String.self, forKey: .address)
+        let address = Address(addressValue)
+        let logoUrl = try values.decode(String.self, forKey: .logoUrl)
+        let `default` = try values.decode(Bool.self, forKey: .default)
+        let token = Token(code: code, name: name, decimals: decimals, address: address, logoUrl: logoUrl)
+        let status: TokenListItemStatus = `default` ? .whitelisted : .regular
+        self.init(token: token, status: status)
+    }
+
+}
+
+public struct TokenList {
 
     public var results: [TokenListItem]
 
@@ -72,3 +76,5 @@ public struct TokenList: Decodable {
     }
 
 }
+
+extension TokenList: Decodable {}
