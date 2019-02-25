935780bd6c7465227ec2be7f5aedb61155c276de
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-02-25 14:17:43 +0100

    GH-580 moved files around

---
+++ SafeUIKit/SafeAppUI/AppFlow/MainFlow/Menu/ConnectBrowserExtension/ConnectBrowserExtensionFlowCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ConnectBrowserExtensionFlowCoordinator: FlowCoordinator {
+
+    override func setUp() {
+        super.setUp()
+        push(UIViewController())
+    }
+
+}
