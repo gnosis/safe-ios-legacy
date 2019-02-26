e8513e9986ad5d3329e33eb7d0a1c93b2d4ca4ee
Valeriy Van <github@w7software.com>
2019-02-26 16:27:56 +0100

    Makes class RetryWithIncreasingDelay final

--- MultisigWallet/MultisigWalletDomainModel/Wallet/RetryWithIncreasingDelay.swift
+++ MultisigWallet/MultisigWalletDomainModel/Wallet/RetryWithIncreasingDelay.swift
@@ -8,7 +8,7 @@ import Common
 /// Retries a closure while it keeps throwing errors until either maxAttempts reached, or closure won't throw.
 /// Every retry is delayed by linearly increasing time interval, calculated as (attemptNumber * startDelay).
 /// When maximum attempts count is reached, while closure is still throwing, last thrown error is rethrown.
-public class RetryWithIncreasingDelay<T> {
+final public class RetryWithIncreasingDelay<T> {
 
     private let main: (Int) throws -> T
     private let maxAttempts: Int
