8649e11af698e348125ff04fe474e6a00e8efb7b
Valeriy Van <github@w7software.com>
2019-03-03 12:44:17 +0100

    Get rid of return in closure to please linter

--- MultisigWallet/MultisigWalletDomainModel/Wallet/RetryWithIncreasingDelayTests.swift
+++ MultisigWallet/MultisigWalletDomainModel/Wallet/RetryWithIncreasingDelayTests.swift
@@ -29,9 +29,7 @@ class RetryWithIncreasingDelayTests: XCTestCase {
     }
 
     func test_whenFinished_thenReturnsValue() throws {
-        let retry = RetryWithIncreasingDelay(maxAttempts: 1) { _ in
-            return true
-        }
+        let retry = RetryWithIncreasingDelay(maxAttempts: 1) { true }
         XCTAssertTrue(try retry.start())
     }
 
