e273df955a1fc1a432697549fa6c2f6fd634e24e
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-05-24 09:57:34 +0200

    GH-790 using enum for localized strings

--- SafeUIKit/SafeAppUI/MainFlow/OnboardingFlow/SetupSafeFlow/NewSafeFlow/OnboardingCreationFeeViewController.swift
+++ SafeUIKit/SafeAppUI/MainFlow/OnboardingFlow/SetupSafeFlow/NewSafeFlow/OnboardingCreationFeeViewController.swift
@@ -11,6 +11,16 @@ class OnboardingCreationFeeViewController: CardViewController {
     let feeRequestView = FeeRequestView()
     let addressDetailView = AddressDetailView()
 
+    enum Strings {
+
+        static let title = LocalizedString("create_safe_title", comment: "Create Safe")
+        static let subtitle = LocalizedString("safe_creation_fee", comment: "Safe creation fee")
+        static let subtitleDetail = LocalizedString("network_fee_required", comment: "Network fee required")
+        static let addressExplanation = LocalizedString("this_is_your_permanent_address", comment: "This is address")
+        static let sendOnlyTokenFormatTemplate = LocalizedString("please_send_x", comment: "Please send %")
+
+    }
+
     static func create() -> OnboardingCreationFeeViewController {
         let controller = OnboardingCreationFeeViewController(nibName: String(describing: CardViewController.self),
                                                              bundle: Bundle(for: CardViewController.self))
@@ -22,14 +32,14 @@ class OnboardingCreationFeeViewController: CardViewController {
         embed(view: feeRequestView, inCardSubview: cardHeaderView)
         embed(view: addressDetailView, inCardSubview: cardBodyView)
 
-        navigationItem.title = LocalizedString("create_safe_title", comment: "Create Safe")
+        navigationItem.title = Strings.title
         navigationItem.leftBarButtonItem = .cancelButton(target: self, action: #selector(cancel))
         navigationItem.rightBarButtonItem = .refreshButton(target: self, action: #selector(refresh))
 
-        setSubtitle(LocalizedString("safe_creation_fee", comment: "Safe creation fee"))
-        setSubtitleDetail(LocalizedString("network_fee_required", comment: "Network fee required"))
+        setSubtitle(Strings.subtitle)
+        setSubtitleDetail(Strings.subtitleDetail)
 
-        feeRequestView.feeTextLabel.text = LocalizedString("this_is_your_permanent_address", comment: "This is address")
+        feeRequestView.feeTextLabel.text = Strings.addressExplanation
         feeRequestView.balanceStackView.isHidden = true
         feeRequestView.feeAmountLabel.amount = TokenData.Ether.withBalance(nil)
 
@@ -38,8 +48,7 @@ class OnboardingCreationFeeViewController: CardViewController {
         addressDetailView.shareButton.addTarget(self, action: #selector(share), for: .touchUpInside)
 
         let feeTokenCode = "OWL"
-        let format = LocalizedString("please_send_x", comment: "Please send %")
-        addressDetailView.footnoteLabel.text = String(format: format, feeTokenCode)
+        addressDetailView.footnoteLabel.text = String(format: Strings.sendOnlyTokenFormatTemplate, feeTokenCode)
 
         footerButton.isHidden = true
     }
