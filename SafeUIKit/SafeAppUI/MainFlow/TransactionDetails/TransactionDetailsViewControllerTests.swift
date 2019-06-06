842d3de1e1f512095641b536b997541b4722ad3e
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-06-06 17:55:18 +0200

    GH-828 updated tests

--- SafeUIKit/SafeAppUI/MainFlow/TransactionDetails/TransactionDetailsViewControllerTests.swift
+++ SafeUIKit/SafeAppUI/MainFlow/TransactionDetails/TransactionDetailsViewControllerTests.swift
@@ -41,7 +41,7 @@ class TransactionDetailsViewControllerTests: XCTestCase {
         XCTAssertEqual(controller.transactionStatusView.status, .pending)
         XCTAssertEqual(controller.transactionStatusView.value, controller.string(from: tx.displayDate!))
 
-        XCTAssertEqual(controller.transactionFeeView.amount, tx.feeTokenData.withBalance(-tx.feeTokenData.balance!))
+        XCTAssertEqual(controller.transactionFeeView.amount, tx.feeTokenData.withBalance(-abs(tx.feeTokenData.balance!)))
 
         XCTAssertEqual(controller.viewInExternalAppButton.title(for: .normal), Strings.externalApp)
     }
