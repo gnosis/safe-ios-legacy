7d6b82cc2b50dfffa6fb0524efaffad8cff866c4
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-07-01 18:09:51 +0200

    GH-917 intermediate

---
+++ safe/safe/IntegrationTests/WalletConnectIntegrationTests.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import safe
+import MultisigWalletImplementations
+
+class WalletConnectIntegrationTests: XCTestCase {
+
+    class SendTransactionHandler: RequestHandler {
+
+    }
+
+    class MyServerDelegate: ServerDelegate {
+
+    }
+
+    func test_connection() {
+        let delegate = MyServerDelegate()
+        let server = Server(delegate: delegate)
+        server.register(handler: SendTransactionHandler())
+        let url = URL(string: "")!
+        server.connect(to: url)
+
+        let exp = expectation(description: "wait")
+        waitForExpectations(timeout: 300, handler: nil)
+    }
+
+}
