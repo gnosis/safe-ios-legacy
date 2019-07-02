701a64f79cca3c2565755b94a9ffa7b9ce522784
Andrey Scherbovich <andrey@gnosis.pm>
2019-07-02 13:48:29 +0200

    GH-917 Add SerializerTests; JSONRPC 2.0 changes;

--- safe/safe/IntegrationTests/WalletConnectIntegrationTests.swift
+++ safe/safe/IntegrationTests/WalletConnectIntegrationTests.swift
@@ -8,23 +8,23 @@ import MultisigWalletImplementations
 
 class WalletConnectIntegrationTests: XCTestCase {
 
-    class SendTransactionHandler: RequestHandler {
-
-    }
-
-    class MyServerDelegate: ServerDelegate {
-
-    }
-
-    func test_connection() {
-        let delegate = MyServerDelegate()
-        let server = Server(delegate: delegate)
-        server.register(handler: SendTransactionHandler())
-        let url = URL(string: "")!
-        server.connect(to: url)
-
-        let exp = expectation(description: "wait")
-        waitForExpectations(timeout: 300, handler: nil)
-    }
+//    class SendTransactionHandler: RequestHandler {
+//
+//    }
+//
+//    class MyServerDelegate: ServerDelegate {
+//
+//    }
+//
+//    func test_connection() {
+//        let delegate = MyServerDelegate()
+//        let server = Server(delegate: delegate)
+//        server.register(handler: SendTransactionHandler())
+//        let url = URL(string: "")!
+//        server.connect(to: url)
+//
+//        let exp = expectation(description: "wait")
+//        waitForExpectations(timeout: 300, handler: nil)
+//    }
 
 }
