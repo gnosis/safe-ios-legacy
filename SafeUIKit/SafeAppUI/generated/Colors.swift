b56470d2d7e07c9545cd730643ab26963cdb8e0e
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-07-29 12:59:27 +0200

    GH-990 color update

--- SafeUIKit/SafeAppUI/generated/Colors.swift
+++ SafeUIKit/SafeAppUI/generated/Colors.swift
@@ -77,6 +77,9 @@ struct ColorName {
   /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#ffffff"></span>
   /// Alpha: 100% <br/> (0xffffffff)
   static let snowwhite = ColorName(rgbaValue: 0xffffffff)
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#007aff"></span>
+  /// Alpha: 20% <br/> (0x007aff33)
+  static let systemBlue20 = ColorName(rgbaValue: 0x007aff33)
   /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#ff3b30"></span>
   /// Alpha: 100% <br/> (0xff3b30ff)
   static let tomato = ColorName(rgbaValue: 0xff3b30ff)
