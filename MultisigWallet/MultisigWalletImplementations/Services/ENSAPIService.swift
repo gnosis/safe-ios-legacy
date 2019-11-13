a19dfd87ffbd9f41c8c3781692eb10a02dba83a1
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-11-13 14:58:54 +0100

    GH-1274 added libidn2 and libunistring for IDN UTS 46 funcitons

---
+++ MultisigWallet/MultisigWalletImplementations/Services/ENSAPIService.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+import MultisigWalletDomainModel
+
+public final class ENSAPIService {
+
+    public init() {}
+
+    public func address(for name: String) throws -> Address {
+        preconditionFailure()
+    }
+
+    public func name(for address: Address) throws -> String {
+        preconditionFailure()
+    }
+
+}
---
+++ MultisigWallet/MultisigWalletImplementationsTests/ENSAPIServiceTests.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import MultisigWalletImplementations
+
+class ENSAPIServiceTests: XCTestCase {
+
+}
