cc822a7f44fa98ea626805c4a4d717541238c66a
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-12-10 19:28:01 +0100

    GH-1357 added encoding implementation

--- MultisigWallet/MultisigWalletDomainModelTests/MultiSendContractProxyTests.swift
+++ MultisigWallet/MultisigWalletDomainModelTests/MultiSendContractProxyTests.swift
@@ -29,4 +29,25 @@ class MultiSendContractProxyTests: XCTestCase {
         XCTAssertEqual(decoded[1].data, data2)
     }
 
+    func test_decode_v2() {
+        DomainRegistry.put(service: EncryptionService(), for: EncryptionDomainService.self)
+        let ownerProxy = SafeOwnerManagerContractProxy(.safeAddress)
+        let data1 = ownerProxy.swapOwner(prevOwner: .one, old: .two, new: .three)
+        let data2 = ownerProxy.swapOwner(prevOwner: .two, old: .three, new: .one)
+        let multiSendProxy = MultiSendV2ContractProxy(.zero)
+        let encoded = multiSendProxy.multiSend([(.call, .safeAddress, 0, data1),
+                                                (.call, .safeAddress, 0, data2)])
+        let decoded: [MultiSendTransaction]! = multiSendProxy.decodeMultiSendArguments(from: encoded)
+        XCTAssertNotNil(decoded)
+        XCTAssertEqual(decoded[0].operation, .call)
+        XCTAssertEqual(decoded[0].to, Address(Address.safeAddress.value.lowercased()))
+        XCTAssertEqual(decoded[0].value, 0)
+        XCTAssertEqual(decoded[0].data, data1)
+
+        XCTAssertEqual(decoded[1].operation, .call)
+        XCTAssertEqual(decoded[1].to, Address(Address.safeAddress.value.lowercased()))
+        XCTAssertEqual(decoded[1].value, 0)
+        XCTAssertEqual(decoded[1].data, data2)
+    }
+
 }
